b44eed14684f222cb377fb7d491567171b4b0fb9
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-05-25 22:06:05 +0100

    Allow users generate PDF copy of CV

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,6 +4,7 @@ class Buttons extends Component {
     render() {
         return (
             <div id="buttons-div">
+                <button onClick={() => window.print()}>Generate PDF</button>
                 <button>Useful Articles</button>
             </div>
         )

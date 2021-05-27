8e6e49ceeb457f6a67941410e55625fa633d9898
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-05-27 17:44:45 +0100

    Make languages section editable

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,21 +1,42 @@
+import { Component } from "react";
 import HoverRating from "./HoverRating";
 
-function Languages() {
-    return (
-        <section id="languages-section">
-            <header className="aside-header">Languages</header>
-            <div>
-                <article>
-                    <p className="sub-header">English</p>
-                    <HoverRating defaultRating={1.5} />
-                </article>
-                <article>
-                    <p className="sub-header">French</p>
-                    <HoverRating defaultRating={4} />
-                </article>
-            </div>
-        </section>
-    )
+class Languages extends Component {
+    constructor() {
+        super();
+        this.state = { readOnly: true }
+    }
+
+    handleDoubleClick = () => this.setState({readOnly: false});
+    handleBlur = () => this.setState({readOnly: true});
+
+    render() {
+        return (
+            <section id="languages-section">
+                <header className="aside-header">Languages</header>
+                <div>
+                    <article>
+                        <p className="sub-header">English</p>
+                        <HoverRating 
+                            defaultRating={1.5} 
+                            readOnly={this.state.readOnly} 
+                            handleDoubleClick={this.handleDoubleClick}
+                            handleBlur={this.handleBlur}
+                        />
+                    </article>
+                    <article>
+                        <p className="sub-header">French</p>
+                        <HoverRating 
+                            defaultRating={4} 
+                            readOnly={this.state.readOnly} 
+                            handleDoubleClick={this.handleDoubleClick}
+                            handleBlur={this.handleBlur}
+                        />
+                    </article>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Languages;

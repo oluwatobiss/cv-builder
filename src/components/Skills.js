82d7e525e370240492281918afc06510cf06cb40
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 15:22:54 +0100

    Change class component to a function component that uses the state hook

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,56 +1,19 @@
-import { Component } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import SkillInfo from "./SkillInfo";
 
-class Skills extends Component {
-    constructor() {
-        super();
-        this.state = { 
-            mySkills: [<SkillInfo key={uniqid()} />]
-        }
-    }
-
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}})
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    handleRateDoubleClick = (e) => {
-        const {id} = e.target;
-        this.setState({[id]: {readOnly: false}});
-    }
-    handleRateBlur = (e) => {
-        const {id} = e.currentTarget;
-        this.setState({[id]: {readOnly: true}});
-    }
-
-    handleBtnClick = () => {
-        this.setState({
-            mySkills: [...this.state.mySkills, <SkillInfo key={uniqid()} />]
-        })
-    }
-
-    render() {
-        return (
-            <section id="skills-section">
-                <header className="aside-header">
-                    <span>Skills</span>
-                    <button className="aside-btn" onClick={this.handleBtnClick}>Add More</button>
-                </header>
-                {this.state.mySkills}
-            </section>
-        )
-    }
+function Skills() {
+    const [mySkills, setMySkills] = useState([<SkillInfo key={uniqid()} />]);
+    return (
+        <section id="skills-section">
+            <header className="aside-header">
+                <span>Skills</span>
+                <button className="aside-btn" onClick={() => setMySkills([...mySkills, <SkillInfo key={uniqid()} />])}>
+                    Add More
+                </button>
+            </header>
+            {mySkills}
+        </section>
+    )
 }
-
 export default Skills;

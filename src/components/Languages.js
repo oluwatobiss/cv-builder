2b7a8b418b3754a459e015e22e9d0ca580ed6a41
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 16:47:06 +0100

    Change class component to a function component that uses the state hook

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,56 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import LanguageInfo from "./LanguageInfo";
 
-class Languages extends Component {
-    constructor() {
-        super();
-        this.state = { 
-            myLanguages: [<LanguageInfo key={uniqid()} />]
-        }
-    }
-
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}})
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    handleRateDoubleClick = (e) => {
-        const {id} = e.currentTarget;
-        this.setState({[id]: {...this.state[id], readOnly: false}});
-    }
-    handleRateBlur = (e) => {
-        const {id} = e.currentTarget;
-        this.setState({[id]: {...this.state[id], readOnly: true}});
-    }
-
-    handleBtnClick = () => {
-        this.setState({
-            myLanguages: [...this.state.myLanguages, <LanguageInfo key={uniqid()} />]
-        })
-    }
-
-    render() {
-        return (
-            <section id="languages-section">
-                <header className="aside-header">
-                    <span>Languages</span>
-                    <button className="aside-btn" onClick={this.handleBtnClick}>Add More</button>
-                </header>
-                {this.state.myLanguages}
-            </section>
-        )
-    }
+function Languages() {
+    const [myLanguages, setMyLanguages] = useState([<LanguageInfo key={uniqid()} />]);
+    return (
+        <section id="languages-section">
+            <header className="aside-header">
+                <span>Languages</span>
+                <button className="aside-btn" onClick={() => setMyLanguages([...myLanguages, <LanguageInfo key={uniqid()} />])}>
+                    Add More
+                </button>
+            </header>
+            {myLanguages}
+        </section>
+    )
 }
 
 export default Languages;

4db2c01c637f8c668adda365145ee91255e36aa3
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 15:08:56 +0100

    Change class component to a function component that uses the state hook

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,48 +1,24 @@
-import { Component } from "react";
+import { useState } from "react";
 import faker from "faker";
 import ElementMaker from "./ElementMaker";
 
-class Phone extends Component {
-    constructor() {
-        super();
-        this.state = {
-            myContactTel: {text: faker.phone.phoneNumber(), showInputEle: false},
-        }
-    }
-
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}})
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    render() {
-        return (
-            <div>
-                <p className="subHeader">Phone</p>
-                <ElementMaker name="myContactTel"
-                    mutableTagType="input"
-                    type="tel"
-                    placeholder="Phone"
-                    staticTagType="p"
-                    text={this.state.myContactTel.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.myContactTel.showInputEle}
-                />
-            </div>
-        )
-    }
+function Phone() {
+    const [myContactTel, setMyContactTel] = useState({text: faker.phone.phoneNumber(), showInputEle: false});
+    return (
+        <div>
+            <p className="subHeader">Phone</p>
+            <ElementMaker name="myContactTel"
+                mutableTagType="input"
+                type="tel"
+                placeholder="Phone"
+                staticTagType="p"
+                text={myContactTel.text}
+                handleEleChange={e => setMyContactTel({...myContactTel, text: e.target.value})}
+                handleEleBlur={() => setMyContactTel({...myContactTel, showInputEle: false})}
+                handleEleDoubleClick={() => setMyContactTel({...myContactTel, showInputEle: true})}
+                showInputEle={myContactTel.showInputEle}
+            />
+        </div>
+    )
 }
-
 export default Phone;

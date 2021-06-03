9a8d8fcf381efa66bc5dbb60afb756ced712b199
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 09:42:35 +0100

    Change class component to function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,15 @@
-import { Component } from "react";
 import AboutApp from "./AboutApp";
 import CVPage from "./CVPage";
 import OffPageButtons from "./OffPageButtons";
 
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div id="app-sections">
-        <AboutApp />
-        <CVPage />
-        <OffPageButtons />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div id="app-sections">
+      <AboutApp />
+      <CVPage />
+      <OffPageButtons />
+    </div>
+  )
 }
 
 export default App;

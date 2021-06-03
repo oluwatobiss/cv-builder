64235fd117608c119a7effd31f8a763ddbd68813
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 11:17:10 +0100

    Change class component to a function component that uses the state hook

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,32 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import ExperienceInfo from "./ExperienceInfo";
 
-class Experience extends Component {
-    constructor() {
-        super();
-        this.state = {
-            experienceInfoArray: [<ExperienceInfo key={uniqid()} />]
-        }
-    }
-
-    handleBtnClick = () => {
-        this.setState({
-            experienceInfoArray: [...this.state.experienceInfoArray, <ExperienceInfo key={uniqid()} />]
-        })
-    }
-
-    render() {
-        return (
-            <section id="experience-section">
-                <header className="main-header">
-                    <span>Experience</span>
-                    <button className="header-btn" onClick={this.handleBtnClick}>Add Experience</button>
-                </header>
-                {this.state.experienceInfoArray}
-            </section>
-        )
-    }
+function Experience() {
+    const [experienceInfoArray, setExperienceInfoArray] = useState([<ExperienceInfo key={uniqid()} />]);
+    return (
+        <section id="experience-section">
+            <header className="main-header">
+                <span>Experience</span>
+                <button className="header-btn" onClick={() => setExperienceInfoArray([...experienceInfoArray, <ExperienceInfo key={uniqid()} />])}>
+                    Add Experience
+                </button>
+            </header>
+            {experienceInfoArray}
+        </section>
+    )
 }
 
 export default Experience;

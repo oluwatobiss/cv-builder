e50510cdfb1aff47d4fafa84ecf90ac7d76d9f99
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 10:47:35 +0100

    Change class component to a function component that uses the state hook

--- src/components/NameAndJobTitle.js
+++ src/components/NameAndJobTitle.js
@@ -1,59 +1,37 @@
-import { Component } from "react";
+import { useState } from "react";
 import faker from "faker";
 import ElementMaker from "./ElementMaker";
 
-class NameAndJobTitle extends Component {
-    constructor() {
-        super();
-        this.state = {
-            fullName: {text: `${faker.name.firstName()} ${faker.name.lastName()}`, showInputEle: false},
-            jobTitle: {text: faker.name.jobTitle(), showInputEle: false}
-        };
-    }
+function NameAndJobTitle() {
+    const [fullName, setFullName] = useState({text: `${faker.name.firstName()} ${faker.name.lastName()}`, showInputEle: false});
+    const [jobTitle, setJobTitle] = useState({text: faker.name.jobTitle(), showInputEle: false});
 
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}});
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    render() {
-        return (
-            <div id="name-and-job-title-div">
-                <ElementMaker name="fullName"
-                    mutableTagType="input"
-                    type="text"
-                    placeholder="Full Name"
-                    staticTagType="p"
-                    text={this.state.fullName.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.fullName.showInputEle}
-                />
-                <ElementMaker name="jobTitle"
-                    mutableTagType="input"
-                    type="text"
-                    placeholder="Profession"
-                    staticTagType="p"
-                    text={this.state.jobTitle.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.jobTitle.showInputEle}
-                />
-            </div>
-        )
-    }
+    return (
+        <div id="name-and-job-title-div">
+            <ElementMaker name="fullName"
+                mutableTagType="input"
+                type="text"
+                placeholder="Full Name"
+                staticTagType="p"
+                text={fullName.text}
+                handleEleChange={e => setFullName({...fullName, text: e.target.value})}
+                handleEleBlur={() => setFullName({...fullName, showInputEle: false})}
+                handleEleDoubleClick={() => setFullName({...fullName, showInputEle: true})}
+                showInputEle={fullName.showInputEle}
+            />
+            <ElementMaker name="jobTitle"
+                mutableTagType="input"
+                type="text"
+                placeholder="Profession"
+                staticTagType="p"
+                text={jobTitle.text}
+                handleEleChange={(e) => setJobTitle({...jobTitle, text: e.target.value})}
+                handleEleBlur={() => setJobTitle({...jobTitle, showInputEle: false})}
+                handleEleDoubleClick={() => setJobTitle({...jobTitle, showInputEle: true})}
+                showInputEle={jobTitle.showInputEle}
+            />
+        </div>
+    )
 }
 
 export default NameAndJobTitle;

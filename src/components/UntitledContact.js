3221d45cfcea1f27768a4032c21c7e3a45eded84
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 15:14:33 +0100

    Change class component to a function component that uses the state hook

--- src/components/UntitledContact.js
+++ src/components/UntitledContact.js
@@ -1,58 +1,34 @@
-import { Component } from "react";
+import { useState } from "react";
 import ElementMaker from "./ElementMaker";
 
-class UntitledContact extends Component {
-    constructor() {
-        super();
-        this.state = {
-            subHeader: {text: "Title", showInputEle: false},
-            myCustomContactText: {text: "Contact Details", showInputEle: false},
-        }
-    }
-
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}})
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    render() {
-        return (
-            <article>
-                <ElementMaker name="subHeader"
-                    mutableTagType="input"
-                    type="text"
-                    placeholder="Label"
-                    staticTagType="p"
-                    text={this.state.subHeader.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.subHeader.showInputEle}
-                />
-                <ElementMaker name="myCustomContactText"
-                    mutableTagType="input"
-                    type="text"
-                    placeholder="Link"
-                    staticTagType="p"
-                    text={this.state.myCustomContactText.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.myCustomContactText.showInputEle}
-                />
-            </article>
-        )
-    }
+function UntitledContact() {
+    const [subHeader, setSubHeader] = useState({text: "Title", showInputEle: false});
+    const [myCustomContactText, setMyCustomContactText] = useState({text: "Contact Details", showInputEle: false});
+    return (
+        <article>
+            <ElementMaker name="subHeader"
+                mutableTagType="input"
+                type="text"
+                placeholder="Label"
+                staticTagType="p"
+                text={subHeader.text}
+                handleEleChange={e => setSubHeader({...subHeader, text: e.target.value})}
+                handleEleBlur={() => setSubHeader({...subHeader, showInputEle: false})}
+                handleEleDoubleClick={() => setSubHeader({...subHeader, showInputEle: true})}
+                showInputEle={subHeader.showInputEle}
+            />
+            <ElementMaker name="myCustomContactText"
+                mutableTagType="input"
+                type="text"
+                placeholder="Link"
+                staticTagType="p"
+                text={myCustomContactText.text}
+                handleEleChange={e => setMyCustomContactText({...myCustomContactText, text: e.target.value})}
+                handleEleBlur={() => setMyCustomContactText({...myCustomContactText, showInputEle: false})}
+                handleEleDoubleClick={() => setMyCustomContactText({...myCustomContactText, showInputEle: true})}
+                showInputEle={myCustomContactText.showInputEle}
+            />
+        </article>
+    )
 }
-
 export default UntitledContact;

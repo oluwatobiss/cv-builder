102a01c5b7b8c79cbc56456f188821620c108954
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 12:48:24 +0100

    Change class component to a function component that uses the state hook

--- src/components/EducationInfo.js
+++ src/components/EducationInfo.js
@@ -1,120 +1,98 @@
-import { Component } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 import ElementMaker from "./ElementMaker";
 import Achievement from "./Achievement";
 
-class Education extends Component {
-    constructor() {
-        super();
-        this.state = {
-            specialty: {text: "Business Administration", showInputEle: false},
-            mySchoolName: {text: "University of California", showInputEle: false},
-            mySchoolCity: {text: "Berkeley", showInputEle: false},
-            startYear: {text: 2008, showInputEle: false},
-            endYear: {text: 2012, showInputEle: false},
-            myAchievements: [<Achievement key={uniqid()} />, <Achievement key={uniqid()} />],
-        }
-    }
+function Education() {
+    const [specialty, setSpecialty] = useState({text: "Business Administration", showInputEle: false});
+    const [mySchoolName, setMySchoolName] = useState({text: "University of California", showInputEle: false});
+    const [mySchoolCity, setMySchoolCity] = useState({text: "Berkeley", showInputEle: false});
+    const [startYear, setStartYear] = useState({text: 2008, showInputEle: false});
+    const [endYear, setEndYear] = useState({text: 2012, showInputEle: false});
+    const [myAchievements, setMyAchievements] = useState([<Achievement key={uniqid()} />, <Achievement key={uniqid()} />]);
 
-    handleEleChange = (e) => {
+    const handleNumEleChange = e => {
         const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}});
+        if (Number(value) !== 0) {
+            switch (name) {
+                case "startYear": setStartYear({...startYear, text: value}); break;
+                case "endYear": setEndYear({...endYear, text: value}); break;
+                default: console.error("Issue with this element's name attribute");
+            }
+        } else { alert("Invalid Input: Only Numbers Allowed.") }
     }
 
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    handleNumEleChange = (e) => {
-        const {name, value} = e.target;
-        Number(value) !== 0 ?
-        this.setState({[name]: {...this.state[name], text: value}}) :
-        alert("Invalid Input: Only Numbers Allowed.");
-    }
-
-    handleBtnClick = () => {
-        this.setState({
-            myAchievements: [...this.state.myAchievements, <Achievement key={uniqid()} />]
-        })
-    }
-
-    render() {
-        return (
-            <>
-                <div className="specialty-div">
-                    <ElementMaker name="specialty"
+    return (
+        <>
+            <div className="specialty-div">
+                <ElementMaker name="specialty"
+                    mutableTagType="input"
+                    type="text"
+                    placeholder="Degree"
+                    staticTagType="span"
+                    text={specialty.text}
+                    handleEleChange={e => setSpecialty({...specialty, text: e.target.value})}
+                    handleEleBlur={() => setSpecialty({...specialty, showInputEle: false})}
+                    handleEleDoubleClick={() => setSpecialty({...specialty, showInputEle: true})}
+                    showInputEle={specialty.showInputEle}
+                />
+                <button className="header-btn" onClick={() => setMyAchievements([...myAchievements, <Achievement key={uniqid()} />])}>
+                    Add Achievement
+                </button>
+            </div>
+            <p>
+                <ElementMaker name="mySchoolName"
+                    mutableTagType="input"
+                    type="text"
+                    placeholder="School/Institution"
+                    staticTagType="span"
+                    text={mySchoolName.text}
+                    handleEleChange={e => setMySchoolName({...mySchoolName, text: e.target.value})}
+                    handleEleBlur={() => setMySchoolName({...mySchoolName, showInputEle: false})}
+                    handleEleDoubleClick={() => setMySchoolName({...mySchoolName, showInputEle: true})}
+                    showInputEle={mySchoolName.showInputEle}
+                />
+                <span>, </span>
+                <ElementMaker name="mySchoolCity"
+                    mutableTagType="input"
+                    type="text"
+                    placeholder="City"
+                    staticTagType="span"
+                    text={mySchoolCity.text}
+                    handleEleChange={e => setMySchoolCity({...mySchoolCity, text: e.target.value})}
+                    handleEleBlur={() => setMySchoolCity({...mySchoolCity, showInputEle: false})}
+                    handleEleDoubleClick={() => setMySchoolCity({...mySchoolCity, showInputEle: true})}
+                    showInputEle={mySchoolCity.showInputEle}
+                />
+            </p>
+            <p>
+                <em>
+                    <ElementMaker name="startYear"
                         mutableTagType="input"
-                        type="text"
-                        placeholder="Degree"
+                        type="number"
                         staticTagType="span"
-                        text={this.state.specialty.text}
-                        handleEleChange={this.handleEleChange}
-                        handleEleBlur={this.handleEleBlur}
-                        handleEleDoubleClick={this.handleEleDoubleClick}
-                        showInputEle={this.state.specialty.showInputEle}
+                        text={startYear.text}
+                        handleEleChange={handleNumEleChange}
+                        handleEleBlur={() => setStartYear({...startYear, showInputEle: false})}
+                        handleEleDoubleClick={() => setStartYear({...startYear, showInputEle: true})}
+                        showInputEle={startYear.showInputEle}
                     />
-                    <button className="header-btn" onClick={this.handleBtnClick}>Add Achievement</button>
-                </div>
-                <p>
-                    <ElementMaker name="mySchoolName"
+                    <span> - </span>
+                    <ElementMaker name="endYear"
                         mutableTagType="input"
-                        type="text"
-                        placeholder="School/Institution"
+                        type="number"
                         staticTagType="span"
-                        text={this.state.mySchoolName.text}
-                        handleEleChange={this.handleEleChange}
-                        handleEleBlur={this.handleEleBlur}
-                        handleEleDoubleClick={this.handleEleDoubleClick}
-                        showInputEle={this.state.mySchoolName.showInputEle}
+                        text={endYear.text}
+                        handleEleChange={handleNumEleChange}
+                        handleEleBlur={() => setEndYear({...endYear, showInputEle: false})}
+                        handleEleDoubleClick={() => setEndYear({...endYear, showInputEle: true})}
+                        showInputEle={endYear.showInputEle}
                     />
-                    <span>, </span>
-                    <ElementMaker name="mySchoolCity"
-                        mutableTagType="input"
-                        type="text"
-                        placeholder="City"
-                        staticTagType="span"
-                        text={this.state.mySchoolCity.text}
-                        handleEleChange={this.handleEleChange}
-                        handleEleBlur={this.handleEleBlur}
-                        handleEleDoubleClick={this.handleEleDoubleClick}
-                        showInputEle={this.state.mySchoolCity.showInputEle}
-                    />
-                </p>
-                <p>
-                    <em>
-                        <ElementMaker name="startYear"
-                            mutableTagType="input"
-                            type="number"
-                            staticTagType="span"
-                            text={this.state.startYear.text}
-                            handleEleChange={this.handleNumEleChange}
-                            handleEleBlur={this.handleEleBlur}
-                            handleEleDoubleClick={this.handleEleDoubleClick}
-                            showInputEle={this.state.startYear.showInputEle}
-                        />
-                        <span> - </span>
-                        <ElementMaker name="endYear"
-                            mutableTagType="input"
-                            type="number"
-                            staticTagType="span"
-                            text={this.state.endYear.text}
-                            handleEleChange={this.handleNumEleChange}
-                            handleEleBlur={this.handleEleBlur}
-                            handleEleDoubleClick={this.handleEleDoubleClick}
-                            showInputEle={this.state.endYear.showInputEle}
-                        />
-                    </em>
-                </p>
-                <ul>{this.state.myAchievements}</ul>
-            </>
-        )
-    }
+                </em>
+            </p>
+            <ul>{myAchievements}</ul>
+        </>
+    )
 }
 
 export default Education;

7276301ae43342cf98b4c999a97ec5b11aedc3c5
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-03 15:04:40 +0100

    Change class component to a function component that uses the state hook

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,87 +1,64 @@
-import { Component } from "react";
+import { useState } from "react";
 import faker from "faker";
 import ElementMaker from "./ElementMaker";
 
-class Address extends Component {
-    constructor() {
-        super();
-        this.state = {
-            myContactStreetName: {text: `73 ${faker.address.streetName()}`, showInputEle: false},
-            myContactCityName: {text: faker.address.cityName(), showInputEle: false},
-            myContactZipCode: {text: faker.address.zipCode(), showInputEle: false},
-            myContactCountry: {text: faker.address.country(), showInputEle: false},
-        }
-    }
+function Address() {
+    const [myContactStreetName, setMyContactStreetName] = useState({text: `73 ${faker.address.streetName()}`, showInputEle: false});
+    const [myContactCityName, setMyContactCityName] = useState({text: faker.address.cityName(), showInputEle: false});
+    const [myContactZipCode, setMyContactZipCode] = useState({text: faker.address.zipCode(), showInputEle: false});
+    const [myContactCountry, setMyContactCountry] = useState({text: faker.address.country(), showInputEle: false});
 
-    handleEleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: {...this.state[name], text: value}})
-    }
-
-    handleEleBlur = (e) => {
-        const {name} = e.target;
-        this.setState({[name]: {...this.state[name], showInputEle: false}});
-    }
-
-    handleEleDoubleClick = (e) => {
-        const {className} = e.target;
-        this.setState({[className]: {...this.state[className], showInputEle: true}});
-    }
-
-    render() {
-        return (
-            <div>
-                <p className="subHeader">Address</p>
-                <ElementMaker name="myContactStreetName"
+    return (
+        <div>
+            <p className="subHeader">Address</p>
+            <ElementMaker name="myContactStreetName"
+                mutableTagType="input"
+                type="text"
+                placeholder="Street Name"
+                staticTagType="p"
+                text={myContactStreetName.text}
+                handleEleChange={e => setMyContactStreetName({...myContactStreetName, text: e.target.value})}
+                handleEleBlur={() => setMyContactStreetName({...myContactStreetName, showInputEle: false})}
+                handleEleDoubleClick={() => setMyContactStreetName({...myContactStreetName, showInputEle: true})}
+                showInputEle={myContactStreetName.showInputEle}
+            />
+            <p>
+                <ElementMaker name="myContactCityName"
                     mutableTagType="input"
                     type="text"
-                    placeholder="Street Name"
-                    staticTagType="p"
-                    text={this.state.myContactStreetName.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.myContactStreetName.showInputEle}
+                    placeholder="City"
+                    staticTagType="span"
+                    text={myContactCityName.text}
+                    handleEleChange={e => setMyContactCityName({...myContactCityName, text: e.target.value})}
+                    handleEleBlur={() => setMyContactCityName({...myContactCityName, showInputEle: false})}
+                    handleEleDoubleClick={() => setMyContactCityName({...myContactCityName, showInputEle: true})}
+                    showInputEle={myContactCityName.showInputEle}
                 />
-                <p>
-                    <ElementMaker name="myContactCityName"
-                        mutableTagType="input"
-                        type="text"
-                        placeholder="City"
-                        staticTagType="span"
-                        text={this.state.myContactCityName.text}
-                        handleEleChange={this.handleEleChange}
-                        handleEleBlur={this.handleEleBlur}
-                        handleEleDoubleClick={this.handleEleDoubleClick}
-                        showInputEle={this.state.myContactCityName.showInputEle}
-                    />
-                    <span>, </span>
-                    <ElementMaker name="myContactZipCode"
-                        mutableTagType="input"
-                        type="text"
-                        placeholder="Zip Code"
-                        staticTagType="span"
-                        text={this.state.myContactZipCode.text}
-                        handleEleChange={this.handleEleChange}
-                        handleEleBlur={this.handleEleBlur}
-                        handleEleDoubleClick={this.handleEleDoubleClick}
-                        showInputEle={this.state.myContactZipCode.showInputEle}
-                    />
-                </p>
-                <ElementMaker name="myContactCountry"
+                <span>, </span>
+                <ElementMaker name="myContactZipCode"
                     mutableTagType="input"
                     type="text"
-                    placeholder="Country"
-                    staticTagType="p"
-                    text={this.state.myContactCountry.text}
-                    handleEleChange={this.handleEleChange}
-                    handleEleBlur={this.handleEleBlur}
-                    handleEleDoubleClick={this.handleEleDoubleClick}
-                    showInputEle={this.state.myContactCountry.showInputEle}
+                    placeholder="Zip Code"
+                    staticTagType="span"
+                    text={myContactZipCode.text}
+                    handleEleChange={e => setMyContactZipCode({...myContactZipCode, text: e.target.value})}
+                    handleEleBlur={() => setMyContactZipCode({...myContactZipCode, showInputEle: false})}
+                    handleEleDoubleClick={() => setMyContactZipCode({...myContactZipCode, showInputEle: true})}
+                    showInputEle={myContactZipCode.showInputEle}
                 />
-            </div>
-        )
-    }
+            </p>
+            <ElementMaker name="myContactCountry"
+                mutableTagType="input"
+                type="text"
+                placeholder="Country"
+                staticTagType="p"
+                text={myContactCountry.text}
+                handleEleChange={e => setMyContactCountry({...myContactCountry, text: e.target.value})}
+                handleEleBlur={() => setMyContactCountry({...myContactCountry, showInputEle: false})}
+                handleEleDoubleClick={() => setMyContactCountry({...myContactCountry, showInputEle: true})}
+                showInputEle={myContactCountry.showInputEle}
+            />
+        </div>
+    )
 }
-
 export default Address;
